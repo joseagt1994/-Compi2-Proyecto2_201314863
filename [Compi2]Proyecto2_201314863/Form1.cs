e44de666bc266cd8c0dbf8fa81ae15477d67db00
joseagt1994 <josegt10100@gmail.com>
2017-10-25 04:26:44 -0600

    Inicio del Proyecto 25/10

---
+++ [Compi2]Proyecto2_201314863/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Compi2_Proyecto2_201314863
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
